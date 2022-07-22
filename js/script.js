7a47e9605d14d909bcc883edcf47b12b0305d161
Lammemr <95485384+Lammemr@users.noreply.github.com>
2022-07-22 08:47:56 -0400

    Update Js
    Clean up, comments

--- js/script.js
+++ js/script.js
@@ -1,46 +1,31 @@
-/*
-Treehouse Techdegree: Data Pagination and Filtering
-*/
-
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab
-   Reach out in your Slack community if you have questions
-*/
-
 /*global variables*/
 const itemsPerPage = 9; /* per example screenshots */
-
-const studentList = document.querySelector('.student-list'); /* grab student list and place in variable
+const studentList = document.querySelector('.student-list'); /* grab student list and place in variable */
 
 /*
-Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
 function showPage(list, page) {
 
-   let startIndex = ( page * 9)-9;
+   let startIndex = ( page * itemsPerPage)-itemsPerPage;
+   let endIndex = page * itemsPerPage;
 
-   let endIndex = page * 9;
-
-   studentList.innerHTML = '';
-
-   for (let i = 0; i < list.length; i++) {
+   studentList.innerHTML = ''; /* create a space for the below data to be appended to */
+ 
+   for (let i = 0; i < list.length; i++) {   /* for loop goes through the given list and appends the information into the html file */
       if (i >= startIndex && i < endIndex) {
          studentList.insertAdjacentHTML('beforeend', `
-         <li class="student-item cf">
-         <div class="student-details">
-           <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
-           <h3>${list[i].name.first} ${list[i].name.last}</h3>
-           <span class="email">${list[i].email}</span>
-         </div>
-         <div class="joined-details"> 
-           <span class="date">${list[i].registered.date}</span>
-         </div>
-       </li>
+            <li class="student-item cf">
+            <div class="student-details">
+            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details"> 
+            <span class="date">${list[i].registered.date}</span>
+            </div>
+            </li>
          `);
 
       }
@@ -51,19 +36,17 @@ function showPage(list, page) {
 };
 
 /*
-Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
 function addPagination(list) {
 
-   let numOfPages = Math.ceil(list.length / itemsPerPage);
+   let numOfPages = Math.ceil(list.length / itemsPerPage); /* Calculate number of pages based on list length */
+   let linkList = document.querySelector('.link-list'); 
 
-   let linkList = document.querySelector('.link-list');
+   linkList.innerHTML = ''; /* create a space for the buttons to be appended to */
 
-   linkList.innerHTML = '';
-
-   for (let i = 1; i <= numOfPages; i++) {
+   for (let i = 1; i <= numOfPages; i++) { /* append a button with the below number, starting with 1 to the html */
       linkList.insertAdjacentHTML('beforeend',`
          <li>
             <button type="button">${i}</button>
@@ -71,18 +54,18 @@ function addPagination(list) {
       `);
    };
 
-   const typeBtn = document.querySelector('[type = "button"]')
+   const typeBtn = document.querySelector('[type = "button"]') /* grab first button and place in variable */
 
-   typeBtn.className = 'active';
+   typeBtn.className = 'active'; /* first always starts as active */
 
-   linkList.addEventListener('click', (e) => {
+   linkList.addEventListener('click', (e) => { /* on button click, give target button 'active' class, remove class from previous active button */
       if (e.target.tagName === 'BUTTON') {
 
-         const lastActive = document.querySelector('.active');
-         const newActive = e.target;
+         const lastActive = document.querySelector('.active'); /* place current active button in variable */
+         const newActive = e.target; /* place new button that was clicked into variable */
 
-         newActive.className = 'active';
-         lastActive.className = '';
+         newActive.className = 'active'; /* changing target to active */
+         lastActive.className = ''; /* remove active from previous button */
 
          let openPage = newActive.textContent;
          showPage(list, openPage);
